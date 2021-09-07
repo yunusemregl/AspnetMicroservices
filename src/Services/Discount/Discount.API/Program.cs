8a899ae9582c17be347868120e2dee584c5fb44b
Yunus Emre Göl <emre.gol@icrontech.com>
2021-09-07 11:27:04 +0300

    Implemented Discount.API, postgresql migrations and containerization of discount.api and postgresql

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,29 @@
+using Discount.API.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<Program>();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
